693aa47a6786ce058d8e590bc9cd3fd1ff602c94
May Syi Ang <maysyian@usc.edu>
2025-04-27 02:23:10 -0700

    Update client_new_ultrasonic.py

--- http_client_server_code/client_new_ultrasonic.py
+++ http_client_server_code/client_new_ultrasonic.py
@@ -1,5 +1,10 @@
 import serial
+import time
 import adafruit_us100
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 us100 = adafruit_us100.US100(uart)
-print("Distance: ", us100.distance)
+
+while True:
+  obj_dist = us100.distance
+  print("Distance: ", obj_dist)
+  time.sleep(0.5)
