64ec66d7b4592db3ec60b00a9f926da23f2fc34d
May Syi Ang <maysyian@usc.edu>
2025-04-27 02:49:41 -0700

    Update client_new_ultrasonic.py

--- http_client_server_code/client_new_ultrasonic.py
+++ http_client_server_code/client_new_ultrasonic.py
@@ -6,10 +6,10 @@ from gpiozero import PWMOutputDevice
 
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1) # Need to configure serial port! (Refer to links in IoT tab group)
 us100 = adafruit_us100.US100(uart)
-buzzer = Buzzer(27) # Represents GPIO17
-# buzzer = PWMOutputDevice(17) # If buzzer is passive
-# buzzer.frequency = 1000  # Sets a 1000 Hz tone
-buzzer.on()
+# buzzer = Buzzer(27) # Represents GPIO27
+buzzer = PWMOutputDevice(27) # If buzzer is passive
+buzzer.frequency = 1000  # Sets a 1000 Hz tone
+buzzer.value = 0.5 # Turn on (duty cycle 50%) for passive buzzer
 
 while True:
   obj_dist = us100.distance
