c2d65ac2c8d0e0489604ee67774ae87ebcf13d16
Le Duong <leduong09042004@gmail.com>
2025-04-27 10:09:53 -0700

    added server to run

--- MiDaS-master/run.py
+++ MiDaS-master/run.py
@@ -13,6 +13,68 @@ import numpy as np
 from imutils.video import VideoStream
 from midas.model_loader import default_models, load_model
 
+#----------- Imports for HTTP Flask Server -----------#
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import render_template
+#-----------------------------------------------------#
+
+# #--------- Imports for loading the ML model ----------#
+# import numpy as np
+# import tensorflow as tf
+# import tensorflow.keras as keras
+# from PIL import Image
+# import matplotlib.pyplot as plt
+# #-----------------------------------------------------#
+
+#-------------- Miscellaneous Imports ----------------#
+from PIL import Image
+import argparse
+import json
+from datetime import datetime
+from threading import Lock
+#-----------------------------------------------------#
+
+# ----------------- GLobal Commands ------------------#
+app = Flask('RaspberryPi Mailbox Server') 
+loaded_model = keras.models.load_model('handNums_model-1104.h5') # load the trained ML model
+LED_command = False # global flag variable for LED
+#-----------------------------------------------------#
+
+# --------------------------- Code for HTTP Server ----------------------------#
+# Set up home page for web front end
+# Use http://localhost:8080/ to access front end
+# Change based on whether command is ON or OFF
+@app.route('/')
+def home():
+    global LED_command
+    if LED_command == True:
+        return render_template('frontEnd-ON.html')
+    return render_template('frontEnd-OFF.html')
+    
+# Custom callback function to receive image from RPi
+@app.route('/send_image', methods=['POST'])
+def post_image_callback():
+    # Ensure the content type is JPEG
+    if request.content_type != 'image/jpeg':
+            return jsonify({"error": "Invalid content type. Only JPEG is supported."}), 400
+
+    # Save the Image
+    image_data = request.data
+    save_path = f'input/test_image.jpg'
+    with open(save_path, "wb") as f:
+            f.write(image_data)
+
+    # Call deploy() to predict the image
+    global buzzer_command
+    buzzer_command = command
+
+    # Return a response to client to confirm the request, along with the LED command
+    return jsonify({"message1": f"Image received and saved as {save_path}", "message2":buzzer_command}), 200
+# -------------------------------------------------------------------------------#
+
+
 first_execution = True
 def process(device, model, model_type, image, input_size, target_size, optimize, use_camera):
     """
@@ -145,7 +207,7 @@ def run(input_path, output_path, model_path, model_type="dpt_beit_large_512", op
         os.makedirs(output_path, exist_ok=True)
 
     print("Start processing")
-
+    command = 'straight'
     if input_path is not None:
         while(True):
             if output_path is None:
@@ -235,7 +297,7 @@ def run(input_path, output_path, model_path, model_type="dpt_beit_large_512", op
             time_start = time.time()
             frame_index = 0
             while True:
-                command = ''
+                command = 'straight'
                 frame = video.read()
                 if frame is not None:
                     original_image_rgb = np.flip(frame, 2)  # in [0, 255] (flip required to get RGB)
@@ -323,6 +385,7 @@ def run(input_path, output_path, model_path, model_type="dpt_beit_large_512", op
         print()
 
     print("Finished")
+    return command
 
 
 if __name__ == "__main__":
@@ -391,7 +454,7 @@ if __name__ == "__main__":
     # set torch options
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = True
+    global command = run(args.input_path, args.output_path, args.model_weights, args.model_type, args.optimize, args.side, args.height, args.square, args.grayscale)
+    app.run(debug=True, host='0.0.0.0', port=8080) # Begin the server
 
-    # compute depth maps
-    run(args.input_path, args.output_path, args.model_weights, args.model_type, args.optimize, args.side, args.height,
-        args.square, args.grayscale)
+    
