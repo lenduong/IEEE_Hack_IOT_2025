b4f9f821aaff93ce8699c90e88695362af5c54bc
May Syi Ang <maysyian@usc.edu>
2025-04-27 10:41:10 -0700

    Update test.py

--- http_client_server_code/test.py
+++ http_client_server_code/test.py
@@ -5,9 +5,9 @@ pin = 22
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
+pwm = GPIO.PWM(pin, 1000)  # 1kHz
 
 while True:
   GPIO.output(pin, GPIO.HIGH)
   time.sleep(0.5)
   GPIO.output(pin, GPIO.LOW)
-  time.sleep(0.5)
