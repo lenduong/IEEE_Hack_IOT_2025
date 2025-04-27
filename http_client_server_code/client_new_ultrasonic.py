ad91325ae5df23f033382a834344de83c8ed0b32
May Syi Ang <maysyian@usc.edu>
2025-04-27 02:33:53 -0700

    Update client_new_ultrasonic.py

--- http_client_server_code/client_new_ultrasonic.py
+++ http_client_server_code/client_new_ultrasonic.py
@@ -1,10 +1,18 @@
 import serial
-import time
 import adafruit_us100
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+import time
+from gpiozero import Buzzer
+
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1) # Need to configure serial port! (Refer to links in IoT tab group)
 us100 = adafruit_us100.US100(uart)
+buzzer = Buzzer(17) # Represents GPIO17
 
 while True:
   obj_dist = us100.distance
   print("Distance: ", obj_dist)
   time.sleep(0.5)
+  if distance < 100:
+    buzzer.beep()
+  if distance > 100:
+    buzzer.off()
+    
